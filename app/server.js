dee29d1c26e54d8ab100ff896e76309e33d30f96
unknown <f.berndt99@gmail.com>
2022-06-07 18:40:27 +0200

    basic routes

--- app/server.js
+++ app/server.js
@@ -5,4 +5,20 @@ app.get('/', function (req, res) {
     res.send("Hello world!");
 });
 
+app.get('/diary/:userId', (req, res) => {
+    res.send("GET user" + req.params.userId);
+})
+
+app.post('/diary/:userId', (req, res) => {
+    res.send("POST user" + req.params.userId);
+})
+
+app.put('/diary/:userId', (req, res) => {
+    res.send("PUT user" + req.params.userId);
+})
+
+app.delete('/diary/:userId', (req, res) => {
+    res.send("DELETE user" + req.params.userId);
+})
+
 app.listen(80);
