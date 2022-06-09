6c5ffa11f549c3a4492d43d1ec5166425ed7fe7b
unknown <f.berndt99@gmail.com>
2022-06-09 19:31:03 +0200

    setup nodemon w/ docker, setup pg connection

--- app/server.js
+++ app/server.js
@@ -1,12 +1,42 @@
 var express = require('express');
 var app = express();
 
+const { Pool } = require('pg');
+
+const connectionString = 'postgres://root:root@127.0.0.1:5432/diary';
+
+const pool = new Pool({
+    user: 'root',
+    password: 'root',
+    host: 'postgres',
+    database: 'diary',
+    port: 5432,
+})
+
 app.get('/', function (req, res) {
     res.send("Hello world!");
 });
 
 app.get('/diary/:userId', (req, res) => {
-    res.send("GET user" + req.params.userId);
+
+    let query = 'SELECT * FROM dev.mood_entries WHERE user_id=$1';
+    let values = [req.params.userId];
+
+    console.log(query);
+
+    pool.query(query, values, (err, resp) => {
+        if (err) {
+            console.log(err);
+
+            res.status(500).send(err);
+        } else {
+            console.log(resp);
+
+            res.status(200).send(resp);
+        }
+    });
+
+    //res.send("GET user" + req.params.userId);
 })
 
 app.post('/diary/:userId', (req, res) => {
@@ -14,7 +44,7 @@ app.post('/diary/:userId', (req, res) => {
 })
 
 app.put('/diary/:userId', (req, res) => {
-    res.send("PUT user" + req.params.userId);
+    res.send("Hey user" + req.params.userId);
 })
 
 app.delete('/diary/:userId', (req, res) => {
