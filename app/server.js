c2c1e19c2093b4be316ac2a75b389405c469dbfc
theEpsilon <fabian.berndt@campus.tu-berlin.de>
2022-06-20 13:25:47 +0200

    Nodemon setup and first PG (#5)
    * setup nodemon w/ docker, setup pg connection
    
    * migrated to sequelize, updated db spec
    
    * updated readme
    
    * code refactor
    
    * prevent server from running upon postgres auth failure
    
    * updated db schema image (again)

--- app/server.js
+++ app/server.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var app = express();
+const { Sequelize, DataTypes, Op } = require('sequelize');
+
+const connectionString = 'postgres://root:root@postgres:5432/diary';
+const sequelize = new Sequelize(connectionString);
+
+const Mood = require("./model.js")(sequelize, DataTypes);
+
+sequelize.authenticate().then(() => console.log("\x1b[32m", "Successfully authenticated to PostgreSQL", "\x1b[0m"));
+Mood.sync().then(() => console.log("\x1b[32m", "Successfully synchronized database model.", "\x1b[0m"));
 
 app.get('/', function (req, res) {
     res.send("Hello world!");
 });
 
 app.get('/diary/:userId', (req, res) => {
-    res.send("GET user" + req.params.userId);
+
+    Mood.findAll({
+        where: {
+            user_id: {
+                [Op.eq]: req.params.userId
+            }
+        }
+    }).then((result) => res.status(200).send(result)).catch((err) => res.status(500).send(err))
 })
 
 app.post('/diary/:userId', (req, res) => {
@@ -14,7 +30,7 @@ app.post('/diary/:userId', (req, res) => {
 })
 
 app.put('/diary/:userId', (req, res) => {
-    res.send("PUT user" + req.params.userId);
+    res.send("Hey user" + req.params.userId);
 })
 
 app.delete('/diary/:userId', (req, res) => {
