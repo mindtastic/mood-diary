53c1f7f07a56295e4ccfd8780002b3065bff8971
Josefine Kipke <josi@konnektiv.de>
2022-07-03 21:43:45 +0200

    Created new routes accorrding to  new API spec see #11 belongs to  #14.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,71 +1,75 @@
-var User = require("../db/index.js").User
-var Mood = require("../db/index.js").Mood
 var authMiddleware = require('../middleware/auth.js');
+const { Op } = require("sequelize");
+const db = require("../db");
+// const MoodEntry = db.mood;
+// const User = db.user;
 
 module.exports = app => {
-    app.use(authMiddleware)
+    app.use(authMiddleware);
 
-    // Testing connection endpoint
-    app.get('/', function (req, res) {
-        res.send("Hello world!");
+    // Get  entry by id. Return mood entry content {id, mood_day, mood_type, mood_descr}
+    app.get('/diary/:id', (req, res) => {
+        const id = req.params.id;
+        console.log("Entry id:" + req.params.id);
+
+        db.mood.findByPk(id).then((data) => {
+            res.status(200).send(data)
+        }).catch(err => {
+            res.status(500).send(err)
+        });
     });
 
-    // Get user by userID
-    app.get('/diary/:userId', (req, res) => {
-        const user = req.user;
-        
-        return res.status(200).json({
-            user: user,
-        });
-    })
-
-    // Get all mood entires for user
-    app.get('/diary/:uid', (req, res) => {
-        Mood.findAll({
+    // Get All entries by user id. 
+    app.get('/diary', (req, res) => {
+        const user = req.user.uid;
+        db.mood.findAll({
+            limit: 30,
             where: {
-                uid: {
-                    [Op.eq]: req.params.uid
-                }
+                author_id: user
             }
-        }).then((result) => res.status(200).send(result)).catch((err) => {
-            console.log(err);
+        }).then((data) => {
+            res.status(200).send(data)
+        }).catch(err => {
+            res.status(500).send(err)
         })
-    })
+    });
 
-    // Create mood entry for user_ID
+    // Create a new mood entry.
     app.post('/diary', (req, res) => {
-
-        if (!req.body) {
-            res.status(400).send("Request body undefined");
-            return;
-        }
-
-        let timestamp = req.body.timestamp;
-
-        if (!timestamp) {
-            timestamp = new Date();
-        }
-
-        Mood.create({
-            user_id: req.body.userId,
-            mood_day: timestamp,
+        const user = req.user.uid;
+        // Insert into mood_entries table.
+        db.mood.create({
+            author_id: user,
+            mood_day: req.body.day,
             mood_type: req.body.type,
             mood_descr: req.body.description
-        }).then((new_mood) => res.status(201).send(new_mood)).catch((err) => res.status(500).send(err))
-    })
-    
-    // Delete mood entry by mood_ID
-    app.delete('/diary/:userId', (req, res) => {
+        }).then(() => {
+            res.status(201).send({
+                message: "Created."
+            });
+        }).catch(err => {
+            res.status(500).send(err)
+        });
+    });
 
-        // const id = Number(req.params.productID)
-        // const index = products.findIndex(product => product.id === id)
-        // if (index === -1) {
-        //     return res.status(404).send('Product not found')
-        // }
-        // products.splice(index, 1)
-        // res.status(200).json('Product deleted')
-        res.send("DELETE user" + req.params.userId);
+    // Delete mood entry by id.
+    app.delete('/diary/:id', (req, res) => {
+        const id = Number(req.params.id)
+        
+        // Delete from mood_entries table.
+        db.mood.destroy({
+            where: { id: id }
+        }).then(num => {
+            if (num == 1) {
+                res.status(204).send()
+            } else {
+                res.status(400).send({
+                    message: `Cannot delete Mood Entry with id=${id}. Maybe the Entry was not found!`
+                })
+            }
+        }).catch(err => {
+            res.status(500).send.send(err)
+        })
     })
-
 
 }
