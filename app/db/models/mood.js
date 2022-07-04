50c9eb2f2d1317b029439e49a6f314c08ce1bf70
Josefine Kipke <josi@konnektiv.de>
2022-07-04 13:20:26 +0200

    Added validation #9.

--- app/db/models/mood.js
+++ app/db/models/mood.js
@@ -7,15 +7,28 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     mood_day: {
-      type: DataTypes.DATE,
-      allowNull: false
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        isDate: true,
+      }
     },
     mood_type: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        validateType(type){
+          if(!(type === 'positive' || type === 'negative' || type === 'neutral')){
+            throw new Error("Mood type must be of type 'positive', 'negative' or 'neutral'.");
+          }
+        }
+      }
     },
     mood_descr: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      validate: {
+        isAlphanumeric: true,     // only allow alphanumeric characters
+      }
     }
   }, {
     schema: 'dev',
