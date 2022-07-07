5ccb985e074c1bf4f326e288b44bdeda55fb522b
Romany Ghoubrial <romany.ghoubrial@gmail.com>
2022-07-07 11:06:33 +0200

    Fix linting issues

--- app/middleware/defaultError.js
+++ app/middleware/defaultError.js
@@ -1,9 +1,15 @@
 const notFoundError = (req, res) => {
-  res.status(404).json({ error: "not found"});
-}; 
+  res.status(404).json({ error: 'not found' });
+};
 
+// Express recongizes the error handler by the number of arguments
+// the function accepts, so `next` should be defined even if it is
+// not used inside the handler. ESLint will complain about unused
+// vars, we will disable it by no-unused-vars rule.
+/* eslint-disable no-unused-vars */
 const serverError = (err, req, res, next) => {
   res.status(500).json({ error: err.message });
 };
+/* eslint-enable no-unused-vars */
 
 export { notFoundError, serverError };
