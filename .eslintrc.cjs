c80b0362efdd3b45e4f87c28bee285ecdceeb391
Johannes Gräger <j.graeger@campus.tu-berlin.de>
2022-07-07 11:23:11 +0200

    ESLint config more express.js middleware

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,5 +11,10 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    // Express recongizes the error handler by the number of arguments
+    // the function accepts, so `next` should be defined in middleware
+    // even if it is not used inside the handler. ESLint will complain
+    // about unused vars, so we disable it by no-unused-vars rule.
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
