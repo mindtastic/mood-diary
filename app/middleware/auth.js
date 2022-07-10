eff7093ddcc5fddfc2669d42b3628dd9d62e0aa2
xenon <34280641+zuse168@users.noreply.github.com>
2022-07-10 02:50:34 +0200

    Update auth.js

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -4,16 +4,17 @@ import db from '../db';
 const authMiddleware = (req, res, next) => {
   const userId = req.get('X-User-Id');
   if (!userId) {
-    res.status(401).json({ error: 'Unauthorized ' });
+   // res.status(401).json({ error: 'Unauthorized ' });
+   
     return;
   }
-
-  try {
-    parseUUID(userId);
-  } catch (e) {
-    res.status(401).json({ error: 'Invalid userId' });
-    return;
-  }
+ parseUUID(userId);
+  //try {
+  //  parseUUID(userId);
+ // } catch (e) {
+    //res.status(401).json({ error: 'Invalid userId' });
+   // return;
+  //}
 
   db.user.findOrCreate({
     where: {
