60366815784653a3f7494117f6ee65cc90e98a96
Josefine Kipke <josi@konnektiv.de>
2022-07-10 13:41:01 +0200

    Removed dev schema.

--- db_seed/testdata.sql
+++ db_seed/testdata.sql
@@ -1,22 +1,20 @@
-CREATE SCHEMA IF NOT EXISTS dev;
-
-CREATE TABLE diary.dev.users (
+CREATE TABLE diary.users (
     uid UUID NOT NULL PRIMARY KEY
    
 );
-CREATE TABLE diary.dev.mood_entries (
+CREATE TABLE diary.mood_entries (
     id SERIAL PRIMARY KEY,
     mood_day TIMESTAMPTZ NOT NULL,
     mood_type VARCHAR (50) NOT NULL,
     mood_descr VARCHAR (50),
-    author_id UUID references diary.dev.users(uid) NOT NULL
+    author_id UUID references diary.users(uid) NOT NULL
 );
 
-INSERT INTO diary.dev.users(uid)
+INSERT INTO diary.users(uid)
     VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608');
 
 
-INSERT INTO diary.dev.mood_entries(mood_day, mood_type, mood_descr, author_id)
+INSERT INTO diary.mood_entries(mood_day, mood_type, mood_descr, author_id)
     VALUES ('2016-06-22 19:10:25-07', 'negative', 'fuming','40e6215d-b5c6-4896-987c-f30f3678f608');
 
 
