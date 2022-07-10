8ebf752c326f7134a2ac2435869619fa97c41f91
Fabian Berndt <fabian.berndt@campus.tu-berlin.de>
2022-07-10 14:48:32 +0200

    Fix/surprise (#30)
    * fixed db connection for dev
    
    * Removed dev schema.
    
    * fixes
    
    * added logging (#29)
    
    * advanced logging
    
    Co-authored-by: Josefine Kipke <josi@konnektiv.de>

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,24 +1,31 @@
-import { parse as parseUUID } from 'uuid';
+import log from 'loglevel';
 import db from '../db';
 
 const authMiddleware = (req, res, next) => {
-    const userId = req.get('X-User-Id');
+  const userId = req.get('X-User-Id');
 
-    if (!userId) {
-        next();
-        return null;
-    }
+  log.debug('\x1b[32m', '[Auth]', 'Headers:', req.rawHeaders, '\x1b[0m');
+  log.debug('\x1b[32m', '[Auth]', 'Body:', req.body, '\x1b[0m');
 
-    db.user.findOrCreate({
-        where: {
-            uid: userId,
-        },
-    }).then(([user]) => {
-        req.user = {
-            uid: user.uid,
-        };
-        next();
-    }).catch(next);
+  if (!userId) {
+    next();
+    return null;
+  }
+
+  log.info('\x1b[32m', '[Auth]', 'User ID:', userId, '\x1b[0m');
+
+  db.user.findOrCreate({
+    where: {
+      uid: userId,
+    },
+  }).then(([user]) => {
+    req.user = {
+      uid: user.uid,
+    };
+    next();
+  }).catch(next);
+
+  return null;
 };
 
 export default authMiddleware;
