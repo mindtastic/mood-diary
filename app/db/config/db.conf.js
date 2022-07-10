e6b35ca5bf1b89fd501de7bac52b109cc208f2d7
Johannes Gräger <j.graeger@campus.tu-berlin.de>
2022-07-10 14:53:29 +0200

    Fixed your ESLint ❌ 👀

--- app/db/config/db.conf.js
+++ app/db/config/db.conf.js
@@ -1,18 +1,18 @@
 export default {
-    host: "postgres",
-    username: "root",
-    password: "root",
-    database: "diary",
-    dialect: 'postgres',
-    dialectOptions: {
-        options: {
-            encrypt: true,
-        }
+  host: 'postgres',
+  username: 'root',
+  password: 'root',
+  database: 'diary',
+  dialect: 'postgres',
+  dialectOptions: {
+    options: {
+      encrypt: true,
     },
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
